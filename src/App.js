35ba91e399bbd700c9535b9a4a8fbb0424da5054
CJY <cjy3458@naver.com>
2023-04-27 22:24:11 +0900

    add component

--- src/App.js
+++ src/App.js
@@ -11,29 +11,41 @@ function Header() {
   );
 }
 
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB!
+    </article>
+  );
+}
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Header></Header>
       <Header></Header>
-      <nav>
-        <ol>
-          <li>
-            <a href="/read/1">html</a>
-          </li>
-          <li>
-            <a href="/read/2">css</a>
-          </li>
-          <li>
-            <a href="/read/3">js</a>
-          </li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB!
-      </article>
+
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
