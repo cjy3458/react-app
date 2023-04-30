475558d096a589f396a7d54d79f648825b0dbd15
CJY <cjy3458@naver.com>
2023-04-30 18:25:06 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -5,27 +5,26 @@ function Header(props) {
   console.log("props", props);
   return (
     <header className="App-header">
-      <h1 a href="/">
-        WEB
+      <h1>
+        <a href="/"> {props.title}</a>
       </h1>
     </header>
   );
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a href={"/read/" + t.id}>{t.title}</a>
+      </li>
+    );
+  }
   return (
     <nav>
-      <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-        <li>
-          <a href="/read/3">js</a>
-        </li>
-      </ol>
+      <ol>{lis}</ol>
     </nav>
   );
 }
@@ -40,15 +39,16 @@ function Article(props) {
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ...?" },
+    { id: 2, title: "css", body: "css is ..?" },
+    { id: 3, title: "JS", body: "JS is ..?" },
+  ];
   return (
     <div className="App">
-      <Header title="REACT"></Header>
-      <Nav></Nav>
-      <Article
-        title="Welcome"
-        body="Hello, WEB
-      "
-      ></Article>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
